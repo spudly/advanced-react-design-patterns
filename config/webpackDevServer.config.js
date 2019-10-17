4d47533806e61e0e430b4497ef71b8056bf88689
Stephen John Sorensen <spudly@users.noreply.github.com>
2019-10-17 18:03:47 -0400

    Add files via upload

---
+++ config/webpackDevServer.config.js
@@ -0,0 +1,28 @@
+const ignoredFiles = require('react-dev-utils/ignoredFiles');
+const paths = require('./paths');
+
+const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+const host = process.env.HOST || '0.0.0.0';
+
+module.exports = function(allowedHost) {
+  return {
+    disableHostCheck: true,
+    compress: true,
+    clientLogLevel: 'none',
+    contentBase: paths.appPublic,
+    watchContentBase: true,
+    hot: true,
+    publicPath: '/',
+    quiet: true,
+    watchOptions: {
+      ignored: ignoredFiles(paths.appSrc),
+    },
+    https: protocol === 'https',
+    host,
+    overlay: false,
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    public: allowedHost,
+  };
+};
